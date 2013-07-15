fcfbf62ec1fc8fee039c817320ca87177400e252
Ryan Merchlewitz <rm.main@gmail.com>
2013-07-14 20:32:05 -0500

    Updated CSS for footer, added links and logo.

--- sites/all/modules/footer_sitemap/footer-menu.tpl.php
+++ sites/all/modules/footer_sitemap/footer-menu.tpl.php
@@ -4,4 +4,30 @@
  * Single menu container.
  */
 ?>
-<div class="<?php print $class; ?>"><hr /><?php print render($menu_output); ?></div>
+<div class="<?php print $class; ?>">
+   <hr />
+   <img src="../../dniSite/sites/all/themes/dnisandbox/images/dniLogo.gif" id="dniLogo" />
+   <?php print render($menu_output); ?>
+</div>
+<div id="linksDiv">
+   <ul>
+      <li>Links</li>
+      <li><a href="www.cisco.com">Cisco Systems, Inc</a></li>
+      <li><a href="http://www.speakeasy.net/speedtest/">Speakeasy Speed Test</a></li>
+      <li><a href="http://www.fedex.com/us/track/">FedEx Tracking</a></li>
+      <li><a href="http://www.ups.com/WebTracking/track?loc=en_US&WT.svl=PriNav">UPS Tracking</a></li>
+      <li><a href="http://www.processor.com/">Processor Information</a></li>
+      <li><a href="http://www.informationweek.com/">Information Week</a></li>
+      <li><a href="www.websudoku.com/">Web Sudoku's</a></li>
+   </ul>
+</div>
+<div id="addressDiv">
+   <p>
+      Dedicated Networks, Inc.<br />
+      14000 Unity St. NW<br />
+      Ramsey, MN<br />
+      55303<br />
+      United States
+   </p>
+</div>
+
