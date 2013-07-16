75de509bb1ae3ae59e66100fb56e93236ecb236f
Ryan Merchlewitz <rm.main@gmail.com>
2013-07-16 07:05:34 -0500

    Updated links in footer section to open new page

--- sites/all/modules/footer_sitemap/footer-menu.tpl.php
+++ sites/all/modules/footer_sitemap/footer-menu.tpl.php
@@ -12,13 +12,13 @@
 <div id="linksDiv">
    <ul>
       <li>Links</li>
-      <li><a href="www.cisco.com">Cisco Systems, Inc</a></li>
-      <li><a href="http://www.speakeasy.net/speedtest/">Speakeasy Speed Test</a></li>
-      <li><a href="http://www.fedex.com/us/track/">FedEx Tracking</a></li>
-      <li><a href="http://www.ups.com/WebTracking/track?loc=en_US&WT.svl=PriNav">UPS Tracking</a></li>
-      <li><a href="http://www.processor.com/">Processor Information</a></li>
-      <li><a href="http://www.informationweek.com/">Information Week</a></li>
-      <li><a href="www.websudoku.com/">Web Sudoku's</a></li>
+      <li><a href="http://www.cisco.com" target="_blank">Cisco Systems, Inc</a></li>
+      <li><a href="http://www.speakeasy.net/speedtest/" target="_blank">Speakeasy Speed Test</a></li>
+      <li><a href="http://www.fedex.com/us/track/" target="_blank">FedEx Tracking</a></li>
+      <li><a href="http://www.ups.com/WebTracking/track?loc=en_US&WT.svl=PriNav" target="_blank">UPS Tracking</a></li>
+      <li><a href="http://www.processor.com/" target="_blank">Processor Information</a></li>
+      <li><a href="http://www.informationweek.com/" target="_blank">Information Week</a></li>
+      <li><a href="http://www.websudoku.com/" target="_blank">Web Sudoku's</a></li>
    </ul>
 </div>
 <div id="addressDiv">
@@ -31,3 +31,4 @@
    </p>
 </div>
 
+ 
